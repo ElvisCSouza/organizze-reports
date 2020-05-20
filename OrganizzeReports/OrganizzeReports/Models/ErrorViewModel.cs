2798cf277156c0c9b7ee10fa34702ebfd22bb147
Elvis Souza <elviscristiansouza@gmail.com>
2020-05-20 14:33:53 -0300

    initial commit

---
+++ OrganizzeReports/OrganizzeReports/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OrganizzeReports.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
